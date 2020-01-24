1b89ddb22fceb0af1737686e8bbcc11b0c9bdafa
Sam Keim <skeim235@gmail.com>
2020-01-24 17:58:11 -0500

    Second version

--- src/PigLatinMain.java
+++ src/PigLatinMain.java
@@ -9,62 +9,102 @@ import java.util.regex.*;
 public class PigLatinMain {
 
 	public String toPigLatin(String phrase) {
-		boolean isUpper = phrase.equals(phrase.toUpperCase());
-		//phrase = phrase.toLowerCase();
+		
+		boolean isUpper = phrase.equals(phrase.toUpperCase()); // check if phrase is in all caps, stores boolean and applies to end of loop
+		
+		// Checks for punctuation at end of string, stores to variable, modifies string and appends punctuation to end
+		String regPunct = "[.?!]";
+		String punctuation = "";
+		if (regexInt(phrase, regPunct) > 0) {
+			punctuation = phrase.substring(regexInt(phrase,regPunct));
+			phrase = phrase.substring(0,phrase.length() - regexInt(phrase,regPunct));
+		}
+		
+		//set up arrays, variables, regex
 		String[] phraseArray = phrase.split(" ");
-		String[] vowelArray = {"a", "e", "i", "o", "u", "A", "E", "I", "O", "U"};
 		String[] finalArray = new String[phraseArray.length];
 		String finalSentence = "";
-		String regNumSym = "[a-z]";
+		String regNumSym = "[0-9@#$%^&*<>.,]";
+		String regVowels = "[aeiouAEIOU]";
 		
+		// Checks if string is blank
+		if (phrase.length() < 1) {
+			return "Please try again.";
+		}
+		
+		// Loops through array of words and makes adjustments
 		for (int x = 0; x < phraseArray.length; x++) {
-			if (phraseArray[x].contains(regNumSym)) {
-				return phrase;
+			
+			// Check if there is a symbol or number, leaves the word unmodified
+			if (regexInt(phraseArray[x],regNumSym) > 0) {
+				finalArray[x] = phraseArray[x] + " ";
+				continue;
 			}
-			if (phrase.length() < 1) {
-				return "Please try again.";
-			}
-			int indx = phraseArray[x].length();
-			for (int i = 0; i < vowelArray.length; ++i) {
-					if (phraseArray[x].indexOf(vowelArray[i]) != -1 && phraseArray[x].indexOf(vowelArray[i]) < indx) {
-						indx = phraseArray[x].indexOf(vowelArray[i]);
-					}
-				}
-				
+			
+			// Translates current word in loop to pig latin
+			int indx = regexInt(phraseArray[x], regVowels);
 				if (indx == 0) {
 					finalArray[x] = phraseArray[x] + "way ";
 				} else {
 					finalArray[x] = phraseArray[x].substring(indx) + phraseArray[x].substring(0,indx) + "ay ";
-				}
-				finalSentence += finalArray[x];
-			}
+			}	
+		}
+		
+		// Converts array to string
+		for (String fin : finalArray) {
+			finalSentence += fin;
+		}
+		
+		// Trims off end space
 		finalSentence = finalSentence.trim();
+	
+		// If the submitted phrase was in uppercase, modifies sentence to uppercase (for -ay and -hay suffixes)
 		if (isUpper) {
 			finalSentence = finalSentence.toUpperCase();
 		}
+		
+		// Appends punctuation if available
+		finalSentence += punctuation;
+		
 		return finalSentence;		
 	}
 	
-//	public static int regexVowels(String phrase) {		
-//		Pattern checkRegex = Pattern.compile("[aeiou]");
-//		Matcher regexMatcher = checkRegex.matcher(phrase);
-//		while(regexMatcher.find()) {
-//			return regexMatcher.start();
-//		}
-//		return 0;
-//	}
+	public static int regexInt(String phrase, String theRegex) {	
+		// Honestly I don't understand these objects very well yet
+		Pattern checkRegex = Pattern.compile(theRegex);
+		Matcher regexMatcher = checkRegex.matcher(phrase);
+		
+		// Checks if regex character is available and returns starting index
+		while(regexMatcher.find()) {
+			return regexMatcher.start();
+		}
+		
+		// Returns 0 if unavailable
+		return 0;
+	}
 	
 	public static void main(String[] args) {
 		PigLatinMain mthd = new PigLatinMain();
 		Scanner scnr = new Scanner(System.in);
-		boolean userChoice = true;
+		String userChoice = "Y";
+		
+		// Do loop will ask for phrase, and continue while userChoice begins with "Y"
 		do{
-			System.out.println("Please enter a phrase.");
-			String phrase = scnr.nextLine();
-			System.out.println(mthd.toPigLatin(phrase));
+			if (userChoice.toUpperCase().startsWith("Y")) { // If user chooses "Y"
+				System.out.println("Please enter a phrase.");
+				String phrase = scnr.nextLine();
+				System.out.println(mthd.toPigLatin(phrase));
+			} else if (userChoice.toUpperCase().startsWith("N")) { // Exits if user chooses "N"
+				System.out.println("Thank you, goodbye.");
+				break;
+			} else { // Alerts user if invalid selection is made
+				System.out.println("Please try again.");
+			}
+			
+			// Asks for user to continue
 			System.out.println("Would you like to enter another phrase? (Y/N)");
-			userChoice = scnr.nextLine().toUpperCase().startsWith("Y");
-		} while (userChoice);
-		System.out.println("Thank you, goodbye.");
+			userChoice = scnr.nextLine();
+			
+		} while (true); // I know there has to be a better way to do this, but I do not know it yet
 	}
 }
